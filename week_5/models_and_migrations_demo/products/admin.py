7eb48c43b5898c0d44ad72048b8dd779b556adc0
SURIYALAKSHMI29 <suriyalakshmi413@gmail.com>
2025-10-09 12:56:35 +0530

    customize admin page

--- week_5/models_and_migrations_demo/products/admin.py
+++ week_5/models_and_migrations_demo/products/admin.py
@@ -4,7 +4,13 @@ from django.contrib import admin
 from .models import Product, Order, OrderItem, Customer
 
 # this allows to manipulate them from admin dashboard
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "qty_on_hand")  # Columns in list view
+    search_fields = ("name",)  # Adds a search box
+    list_filter = ("qty_on_hand",)  # Filter sidebar
+    ordering = ("name",)  # Default sorting
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Customer)
